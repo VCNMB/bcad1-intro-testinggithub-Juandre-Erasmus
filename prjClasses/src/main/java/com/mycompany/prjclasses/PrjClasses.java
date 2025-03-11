c0864b5e8722d64f074507f5cae0f4eb02b6acc8
Juandre-Erasmus <ST10472733@vcconnect.edu.za>
2025-03-11 07:15:10 +0200

    variables
    added public variables

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -9,8 +9,21 @@ package com.mycompany.prjclasses;
  * @author lab_services_student
  */
 public class PrjClasses {
+final public double one = 5;
+final public double two = 9;
 
     public static void main(String[] args) {
         System.out.println("Hello World! I just did Github and love Reece now");
+        
+        newClass nc = new newClass(); //creating a new class
+        nc.helloClass(); //have to use variable name nc to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar);
     }
+ 
+                
+      
+        
+        
+    
 }
