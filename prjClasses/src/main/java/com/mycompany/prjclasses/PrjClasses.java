b170e78252f442b59b7cfb691ba74d9cad120ddb
Juandre-Erasmus <ST10472733@vcconnect.edu.za>
2025-03-11 07:17:06 +0200

    removed variables

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -9,9 +9,7 @@ package com.mycompany.prjclasses;
  * @author lab_services_student
  */
 public class PrjClasses {
-final public double one = 5;
-final public double two = 9;
-
+    
     public static void main(String[] args) {
         System.out.println("Hello World! I just did Github and love Reece now");
         
